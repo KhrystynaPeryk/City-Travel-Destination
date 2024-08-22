35063930a03b6ed3b37f4a5da7ae97ca91c00af9
Khrystyna Peryk <kristik991@gmail.com>
2024-08-22 13:17:49 +0400

    green checkbox

--- src/components/home-components/Checkbox.js
+++ src/components/home-components/Checkbox.js
@@ -1,7 +1,7 @@
 const Checkbox = ({label}) => {
     return (
         <div className="flex items-center m-3 ">
-            <input id={label} type="checkbox" value={label} className="w-9 h-9 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+            <input id={label} type="checkbox" value={label} className="w-9 h-9 appearance-none bg-green-100 border border-green-300 rounded focus:ring-green-500 checked:bg-green-600 checked:border-green-600 focus:ring-2 focus:ring-offset-0 checked:ring-0 checked:before:content-['✓'] checked:before:block checked:before:text-white checked:before:text-center checked:before:font-bold checked:before:text-lg"/>
             <label htmlFor={label} className="pl-3 text-sm font-medium text-white-900 dark:text-gray-300">{label}</label>
             </div>
     )
